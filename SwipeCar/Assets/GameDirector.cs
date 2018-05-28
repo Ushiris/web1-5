207fe7d068377d8b5950bb2544878562316647e2
Unknown <umiushicco@gmail.com>
2018-05-28 17:25:46 +0900

    一応完成

---
+++ SwipeCar/Assets/GameDirector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject
+        car,
+        flag,
+        distance;
+	// Use this for initialization
+	void Start ()
+    {
+        car = GameObject.Find("car");
+        flag = GameObject.Find("flag");
+        distance = GameObject.Find("Distance");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float length = flag.transform.position.x - car.transform.position.x;
+        if(length>=0)
+        {
+            distance.GetComponent<Text>().text = "ゴールまで" + length.ToString("F2") + "m";
+        }
+        else
+        {
+            distance.GetComponent<Text>().text = "ゲームオーバー！";
+        }
+    }
+}
